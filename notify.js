e443391769ca78472bd49d91859316816e0566ba
Dayveed021 <boskey021@gmail.com>
2023-01-28 10:49:44 +0100

    notify

--- notify.js
+++ notify.js
@@ -13,9 +13,16 @@ function removeDots(elem) {
    const dot = elem.querySelector('#dot');
    dot.remove();
 }
+
 function read(elem) {
    const read = elem.querySelector('.title');
    read.style.color = 'rgb(147, 157, 174)';
+}
+function readAll() {
+   var elements = document.getElementsByClassName('title'); // get all elements
+   for (var i = 0; i < elements.length; i++) {
+      elements[i].style.color = "rgb(147, 157, 174)";
+   }
 }
 
 function Mcount(count) {
@@ -37,6 +44,7 @@ function clicked() {
    for (var i = 0; i < elements.length; i++) {
       elements[i].style.backgroundColor = "white";
       countSpan.textContent = 0;
+      readAll()
    }
    redDot();
 }
